4f5e61307518435c0e07f75724a258df157bbb90
ganjianping <ganjianping1984@gmail.com>
2025-11-02 22:26:37 +0800

    style(images): show image name as overlay at bottom with transparent background

--- src/pages/Images/ImageCard.tsx
+++ src/pages/Images/ImageCard.tsx
@@ -14,7 +14,7 @@ export const ImageCard = ({ item, onClick }: ImageCardProps) => {
   const cardContent = (
     <figure className="card image-card">
       <img src={imageSource} alt={altText} loading="lazy" className="image-card__image" />
-      <figcaption className="image-card__caption">
+      <figcaption className="image-card__overlay">
         <div className="image-card__title">{item.name ?? item.title ?? 'Untitled image'}</div>
       </figcaption>
     </figure>
